09926e8215590f9aa24f1b45329366e4f5fd3175
asif mohd <itsafakemail400@gmail.com>
2022-12-31 13:17:40 +0530

    hosting error fix

--- src/components/SearchBarPopupMenu.jsx
+++ src/components/SearchBarPopupMenu.jsx
@@ -4,14 +4,16 @@ import { cities } from "../data";
 const SearchBarPopupMenu = ({ setIsSearchbarOpen }) => {
   const [data, setData] = useState(cities.values);
   const [city, setCity] = useState("kollam");
+  const setCityandClose = (e) => {
+    setCity(e.target.textContent);
+    setIsSearchbarOpen(false);
+  };
   return (
     <div className="w-[20em] h-[13em] bg-white border-2 rounded-md absolute top-[3.5em] flex flex-col overflow-y-auto z-[2]">
       {data.map((city, index) => [
         <div
           className=" border-b-2 tracking-wide text-gray-500 hover:bg-[#F8F8F8] cursor-pointer text-left px-6"
-          onClick={(e) => (
-            setCity(e.target.textContent), setIsSearchbarOpen(false)
-          )}
+          onClick={(e) => setCityandClose(e)}
           key={index}
         >
           <h2 className="py-4 ">{city}</h2>
