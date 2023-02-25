38c0e2e543797acc9fde8b9d069b9e1f7b0be82b
asif mohd <itsafakemail400@gmail.com>
2023-02-25 12:44:33 +0530

    small tweak on components index.js export

--- src/components/index.js
+++ src/components/index.js
@@ -4,18 +4,5 @@ import Hero from "./Hero";
 import Options from "./Options";
 import Getapp from "./Getapp";
 import Home from "./Home";
-import RestaurantCreateForm from "./RestaurantCreateForm";
-import CreateMenu from "./CreateMenu";
-import RegisterForm from "./RegisterForm";
 
-export {
-  Footer,
-  Header,
-  Hero,
-  Options,
-  Getapp,
-  Home,
-  RestaurantCreateForm,
-  CreateMenu,
-  RegisterForm,
-};
+export { Footer, Header, Hero, Options, Getapp, Home };
