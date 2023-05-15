36a593f5dd4be34b69f880e1d6ebaa214f30bfd9
asif mohd <itsafakemail400@gmail.com>
2023-05-15 16:42:01 +0530

    changed axios url

--- src/components/utils/axiosInstance.js
+++ src/components/utils/axiosInstance.js
@@ -4,7 +4,7 @@ const clientToken = window.localStorage.getItem("clientToken");
 const backendUrl = process.env.REACT_APP_BACKEND_URL;
 const localUrl = process.env.REACT_APP_LOCAL_URL;
 const api = axios.create({
-  baseURL: localUrl,
+  baseURL: backendUrl,
   headers: { Authorization: `Bearer ${clientToken}` },
 });
 export default api;
